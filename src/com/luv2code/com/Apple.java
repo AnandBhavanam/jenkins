28d1395e8a687777b71322e4a4e55afc34d9bc49
anand47r@gmail.com <anand47r@gmail.com>
2020-08-30 21:07:30 -0400

    test

---
+++ src/com/luv2code/com/Apple.java
@@ -0,0 +1,17 @@
+package com.luv2code.com;
+
+public class Apple {
+	
+//	public static <T extends Number> void m1(T a, T b) {
+//		
+//		return a+b;
+//	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//m1(2,3);
+		
+	}
+
+}
---
+++ src/com/luv2code/com/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.luv2code.com;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+
+}
